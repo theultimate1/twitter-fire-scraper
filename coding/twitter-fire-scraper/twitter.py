fd9a4715a97ba3dd4d6f44de4c98167fc4f9c2f2
HenryFBP <HenryFBP@gmail.com>
2019-02-21 00:53:23 -0600

    clean up documentation

--- coding/twitter-fire-scraper/twitter.py
+++ coding/twitter-fire-scraper/twitter.py
@@ -6,7 +6,7 @@ from pprint import pprint
 import colorama
 import tweepy
 from colorama import Fore
-from tweepy import OAuthHandler
+from tweepy import OAuthHandler, Status
 from textblob import TextBlob
 
 from config import Config
@@ -48,10 +48,13 @@ class TwitterClient(object):
 
     @staticmethod
     def clean_tweet(tweet):
+        # type: (str) -> str
         return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
 
     @staticmethod
     def get_tweet_sentiment(tweet):
+        # type: (str) -> str
+
         # thresholds to classify pos and neg
         POS_THRESH = 0.2
         NEG_THRESH = 0
@@ -67,21 +70,36 @@ class TwitterClient(object):
         return "nope."
 
 
+class MongoDBFileStreamListener(tweepy.StreamListener):
+    """A twitter stream listener that logs flagged tweets to a MongoDB database."""
+
+    # TODO implement it :)
+
+
 # override tweepy.StreamListener to add logic to on_status
 class SimpleFireStreamListener(tweepy.StreamListener):
-    """A naive twitter stream listener that only watches for the phrase "fire"."""
+    """A naive twitter stream listener that only watches for the phrase "fire".
+    This is a simple demo and is best used on the command-line."""
 
     @staticmethod
-    def is_relevant(status):
+    def is_relevant(status, verbose=False):
+        # type: (Status, bool) -> bool
+        """
+        Tells you if a Status is relevant.
+        :param status: The status.
+        :param verbose: Should we print irrelevant statuses?
+        :return: Whether or not the status is relevant.
+        """
 
         text = status.text.encode("UTF-8")
 
         if 'fire' in text:
             return True
 
-        # Show snapshot of irrelevant tweet
-        print("Not relevant: {}".format(text.replace("\n", "\\n").replace("\r", "\\r")[0:50] + "..."))
-        colorama_reset()
+        if verbose:
+            # Show snapshot of irrelevant tweet
+            print("Not relevant: {}".format(text.replace("\n", "\\n").replace("\r", "\\r")[0:50] + "..."))
+            colorama_reset()
 
         return False
 
@@ -106,22 +124,32 @@ if __name__ == "__main__":
     twauth = TwitterAuthentication()
     api = twauth.get_api()
 
-    print("Just searching for 'fire'... Probably not going to get us Chicago fire incidents, "
-          "perhaps will get us SoundCloud tracks.")
-    pprint([(status.text,) for status in tc.api.search("fire")])
 
-    print("Using our simple 'fire' stream listener, let's see what Chicago geotagged tweets have 'fire' in them.")
+    def firesDemo():
 
-    simpleFireStreamListener = SimpleFireStreamListener()
-    simpleFireStream = tweepy.Stream(auth=api.auth, listener=simpleFireStreamListener)
+        print("Just searching for 'fire'... Probably not going to get us Chicago fire incidents, "
+              "perhaps will get us SoundCloud tracks.")
+        pprint([(status.text,) for status in tc.api.search("fire")])
 
-    print("Streaming for 60 seconds.")
-    simpleFireStream.filter(locations=GEOBOX_CHICAGO, async=True)
-    try:
-        time.sleep(60)
-    except KeyboardInterrupt:
-        simpleFireStream.disconnect()
-    finally:
-        simpleFireStream.disconnect()
 
-    print("As you can see, either none or few of these tweets actually contain the word 'fire'.")
+    def geoFiresDemo():
+        print("Using our simple 'fire' stream listener, let's see what Chicago geotagged tweets have 'fire' in them.")
+
+        simpleFireStreamListener = SimpleFireStreamListener()
+        simpleFireStream = tweepy.Stream(auth=api.auth, listener=simpleFireStreamListener)
+
+        print("Streaming for 60 seconds.")
+        simpleFireStream.filter(locations=GEOBOX_CHICAGO, async=True)
+        try:
+            time.sleep(60)
+        except KeyboardInterrupt:
+            simpleFireStream.disconnect()
+        finally:
+            simpleFireStream.disconnect()
+
+        print("As you can see, either none or few of these tweets actually contain the word 'fire'.")
+
+
+    firesDemo()
+
+    geoFiresDemo()
