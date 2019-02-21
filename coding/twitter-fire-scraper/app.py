6ce05098f856d7c6044cad283b560c09984598e4
HenryFBP <HenryFBP@gmail.com>
2019-02-20 20:04:02 -0600

    rename app

--- coding/twitter-fire-scraper/app.py
+++ coding/twitter-fire-scraper/app.py
@@ -5,15 +5,15 @@ from config import Config
 from twitter import TwitterClient
 from util import strtobool
 
-app = Flask(__name__, static_path="/static")
+flask_app = Flask(__name__, static_path="/static")
 twitter_api = TwitterClient()
 
 
-@app.route('/')
+@flask_app.route('/')
 def index():
     return render_template('index.html')
 
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host="127.0.0.1", port=port, debug=True)
+    flask_app.run(host="127.0.0.1", port=port, debug=True)
