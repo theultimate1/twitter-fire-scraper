2952f75ce5303539ff47a8770edbe16b59366850
Henry Post <HenryFBP@gmail.com>
2019-02-26 23:27:11 -0600

    add first demo

--- coding/twitter-fire-scraper/Run-Demo.ps1
+++ coding/twitter-fire-scraper/Run-Demo.ps1
@@ -2,7 +2,123 @@
 This file runs a demo of the Twitter fire scraper functionality for presentation purposes.
 #>
 
-function detectPython2([string]$Verbose) {
+
+Function Trace-Word
+{
+    [Cmdletbinding()]
+    [Alias("Highlight")]
+    Param(
+            [Parameter(ValueFromPipeline=$true, Position=0)] [string[]] $content,
+            [Parameter(Position=1)] 
+            [ValidateNotNull()]
+            [String[]] $words = $(throw "Provide word[s] to be highlighted!")
+    )
+    
+    Begin
+    {
+        
+        $Color = @{       
+                    0='Yellow'      
+                    1='Magenta'     
+                    2='Red'         
+                    3='Cyan'        
+                    4='Green'       
+                    5 ='Blue'        
+                    6 ='DarkGray'    
+                    7 ='Gray'        
+                    8 ='DarkYellow'    
+                    9 ='DarkMagenta'    
+                    10='DarkRed'     
+                    11='DarkCyan'    
+                    12='DarkGreen'    
+                    13='DarkBlue'        
+        }
+
+        $ColorLookup =@{}
+
+        For($i=0;$i -lt $words.count ;$i++)
+        {
+            if($i -eq 13)
+            {
+                $j =0
+            }
+            else
+            {
+                $j = $i
+            }
+
+            $ColorLookup.Add($words[$i],$Color[$j])
+            $j++
+        }
+        
+    }
+    Process
+    {
+    $content | ForEach-Object {
+    
+        $TotalLength = 0
+               
+        $_.split() | `
+        Where-Object {-not [string]::IsNullOrWhiteSpace($_)} | ` #Filter-out whiteSpaces
+        ForEach-Object{
+                        if($TotalLength -lt ($Host.ui.RawUI.BufferSize.Width-10))
+                        {
+                            #"TotalLength : $TotalLength"
+                            $Token =  $_
+                            $displayed= $False
+                            
+                            Foreach($Word in $Words)
+                            {
+                                if($Token -like "*$Word*")
+                                {
+                                    $Before, $after = $Token -Split "$Word"
+                              
+                                        
+                                    #"[$Before][$Word][$After]{$Token}`n"
+                                    
+                                    Write-Host $Before -NoNewline ; 
+                                    Write-Host $Word -NoNewline -Fore Black -Back $ColorLookup[$Word];
+                                    Write-Host $after -NoNewline ; 
+                                    $displayed = $true                                   
+                                    #Start-Sleep -Seconds 1    
+                                    #break  
+                                }
+
+                            } 
+                            If(-not $displayed)
+                            {   
+                                Write-Host "$Token " -NoNewline                                    
+                            }
+                            else
+                            {
+                                Write-Host " " -NoNewline  
+                            }
+                            $TotalLength = $TotalLength + $Token.Length  + 1
+                        }
+                        else
+                        {                      
+                            Write-Host '' #New Line  
+                            $TotalLength = 0 
+
+                        }
+
+                            #Start-Sleep -Seconds 0.5
+                        
+        }
+        Write-Host '' #New Line               
+    }
+    }
+    end
+    {    }
+
+}
+
+
+#Trace-Word -content (Get-Content iis.log) -words "IIS", 's', "exe", "10", 'system'
+
+
+
+function detectPython2() {
     # Detects Python2 command.
 
     $PythonCommand = "python"
@@ -39,5 +155,34 @@ function detectPython2([string]$Verbose) {
 }
 
 
-$PythonCommand = detectPython2;
+$PythonCommand = (detectPython2)[-1] # Detect Python 2 installation.
 
+$PipenvCommand = $PythonCommand + " -m pipenv"
+
+$PipenvRunCommand = $PipenvCommand + " run python"
+
+Write-Output("Ensuring packages are installed...")
+
+Invoke-Expression ($PythonCommand+" -m pip install pipenv")
+
+Invoke-Expression ($PythonCommand+" -m pipenv install")
+
+$userInput = ""
+
+while($userInput -notlike "q") {
+
+    if($userInput -like "c") {
+        cls
+    } elseif($userInput -like "1") {
+        Invoke-Expression($PipenvRunCommand+" tests/interactive/testAllTwitterFire.py") | Trace-Word -words "fire"
+    }
+
+    Write-Host("q) Quits program.")
+    Write-Host("c) Clears the screen.")
+    Write-Host("1) Search all of twitter for 'fire'.")
+    
+    $userInput = Read-Host(" > ")
+
+}
+
+Write-Host("Bye!")
