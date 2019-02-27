533c76c0c140ffa314a7f87b888a7ffb40b61c31
HenryFBP <HenryFBP@gmail.com>
2019-02-27 13:28:48 -0600

    reformat demo.ps1

--- coding/twitter-fire-scraper/Run-Demo.ps1
+++ coding/twitter-fire-scraper/Run-Demo.ps1
@@ -5,113 +5,115 @@ This file runs a demo of the Twitter fire scraper functionality for presentation
 [console]::TreatControlCAsInput = $true # Stop CTRL-C from killing our script.
 
 Function Trace-Word #from <http://ridicurious.com/2018/03/14/highlight-words-in-powershell-console/>
-{ #Trace-Word -content (Get-Content iis.log) -words "IIS", 's', "exe", "10", 'system'
+{
+#Trace-Word -content (Get-Content iis.log) -words "IIS", 's', "exe", "10", 'system'
 
     [Cmdletbinding()]
     [Alias("Highlight")]
     Param(
-            [Parameter(ValueFromPipeline=$true, Position=0)] [string[]] $content,
-            [Parameter(Position=1)]
-            [ValidateNotNull()]
-            [String[]] $words = $(throw "Provide word[s] to be highlighted!")
+        [Parameter(ValueFromPipeline = $true, Position = 0)] [string[]] $content,
+        [Parameter(Position = 1)]
+        [ValidateNotNull()]
+        [String[]] $words = $( throw "Provide word[s] to be highlighted!" )
     )
 
     Begin
     {
 
         $Color = @{
-                    0='Yellow'
-                    1='Magenta'
-                    2='Red'
-                    3='Cyan'
-                    4='Green'
-                    5 ='Blue'
-                    6 ='DarkGray'
-                    7 ='Gray'
-                    8 ='DarkYellow'
-                    9 ='DarkMagenta'
-                    10='DarkRed'
-                    11='DarkCyan'
-                    12='DarkGreen'
-                    13='DarkBlue'
+            0 = 'Yellow'
+            1 = 'Magenta'
+            2 = 'Red'
+            3 = 'Cyan'
+            4 = 'Green'
+            5 = 'Blue'
+            6 = 'DarkGray'
+            7 = 'Gray'
+            8 = 'DarkYellow'
+            9 = 'DarkMagenta'
+            10 = 'DarkRed'
+            11 = 'DarkCyan'
+            12 = 'DarkGreen'
+            13 = 'DarkBlue'
         }
 
-        $ColorLookup =@{}
+        $ColorLookup = @{ }
 
-        For($i=0;$i -lt $words.count ;$i++)
+        For($i = 0; $i -lt $words.count; $i++)
         {
-            if($i -eq 13)
+            if ($i -eq 13)
             {
-                $j =0
+                $j = 0
             }
             else
             {
                 $j = $i
             }
 
-            $ColorLookup.Add($words[$i],$Color[$j])
+            $ColorLookup.Add($words[$i], $Color[$j])
             $j++
         }
 
     }
     Process
     {
-    $content | ForEach-Object {
+        $content | ForEach-Object {
 
-        $TotalLength = 0
+            $TotalLength = 0
 
-        $_.split() | `
-        Where-Object {-not [string]::IsNullOrWhiteSpace($_)} | ` #Filter-out whiteSpaces
-        ForEach-Object{
-                        if($TotalLength -lt ($Host.ui.RawUI.BufferSize.Width-10))
+            $_.split() | `
+        Where-Object { -not [string]::IsNullOrWhiteSpace($_) } | ` #Filter-out whiteSpaces
+            ForEach-Object{
+                if ($TotalLength -lt ($Host.ui.RawUI.BufferSize.Width - 10))
+                {
+                    #"TotalLength : $TotalLength"
+                    $Token = $_
+                    $displayed = $False
+
+                    Foreach ($Word in $Words)
+                    {
+                        if ($Token -like "*$Word*")
                         {
-                            #"TotalLength : $TotalLength"
-                            $Token =  $_
-                            $displayed= $False
-
-                            Foreach($Word in $Words)
-                            {
-                                if($Token -like "*$Word*")
-                                {
-                                    $Before, $after = $Token -Split "$Word"
+                            $Before, $after = $Token -Split "$Word"
 
 
-                                    #"[$Before][$Word][$After]{$Token}`n"
+                            #"[$Before][$Word][$After]{$Token}`n"
 
-                                    Write-Host $Before -NoNewline ;
-                                    Write-Host $Word -NoNewline -Fore Black -Back $ColorLookup[$Word];
-                                    Write-Host $after -NoNewline ;
-                                    $displayed = $true
-                                    #Start-Sleep -Seconds 1
-                                    #break
-                                }
-
-                            }
-                            If(-not $displayed)
-                            {
-                                Write-Host "$Token " -NoNewline
-                            }
-                            else
-                            {
-                                Write-Host " " -NoNewline
-                            }
-                            $TotalLength = $TotalLength + $Token.Length  + 1
-                        }
-                        else
-                        {
-                            Write-Host '' #New Line
-                            $TotalLength = 0
-
+                            Write-Host $Before -NoNewline;
+                            Write-Host $Word -NoNewline -Fore Black -Back $ColorLookup[$Word];
+                            Write-Host $after -NoNewline;
+                            $displayed = $true
+                            #Start-Sleep -Seconds 1
+                            #break
                         }
 
-                            #Start-Sleep -Seconds 0.5
+                    }
+                    If (-not$displayed)
+                    {
+                        Write-Host "$Token " -NoNewline
+                    }
+                    else
+                    {
+                        Write-Host " " -NoNewline
+                    }
+                    $TotalLength = $TotalLength + $Token.Length + 1
+                }
+                else
+                {
+                    Write-Host '' #New Line
+                    $TotalLength = 0
 
+                }
+
+                #Start-Sleep -Seconds 0.5
+
+            }
+            Write-Host '' #New Line
         }
-        Write-Host '' #New Line
-    }
     }
     end
-    {    }
+    {
+    }
 
 }
 
@@ -119,21 +121,29 @@ Function Trace-Word #from <http://ridicurious.com/2018/03/14/highlight-words-in-
 
 
 
-function detectPython2() {
+function detectPython2()
+{
     # Detects Python2 command.
 
     $PythonCommand = "python"
-    $PythonVersion = Invoke-Expression ($PythonCommand+" -V")
+    $PythonVersion = Invoke-Expression ($PythonCommand + " -V")
 
-    if($PythonVersion -like "Python 2") {
+    if ($PythonVersion -like "Python 2")
+    {
 
         Write-Output("Python 2 detected via ``python`` command.")
 
-    } elseif(Get-Command "python2" -ErrorAction SilentlyContinue) { # `python` is not Py2,
+    }
+    elseif(Get-Command "python2" -ErrorAction SilentlyContinue)
+    {
+    # `python` is not Py2,
 
         $PythonCommand = "python2";
         Write-Output("Python 2 detected via ``python2`` command.")
-    } else { # `python2` does not exist.
+    }
+    else
+    {
+    # `python2` does not exist.
 
         Write-Output("``python`` refers to Python 3 and ``python2`` does not exist.`n")
 
@@ -164,20 +174,26 @@ $PipenvRunCommand = $PipenvCommand + " run python"
 
 Write-Output("Ensuring packages are installed...")
 
-Invoke-Expression ($PythonCommand+" -m pip install pipenv")
+Invoke-Expression ($PythonCommand + " -m pip install pipenv")
 
-Invoke-Expression ($PythonCommand+" -m pipenv install")
+Invoke-Expression ($PythonCommand + " -m pipenv install")
 
 $userInput = ""
 
-while($userInput -notlike "q") {
+while ($userInput -notlike "q")
+{
 
-    if($userInput -like "c") {
+    if ($userInput -like "c")
+    {
         Clear-Host
-    } elseif($userInput -like "1") {
-        Invoke-Expression($PipenvRunCommand+" tests/interactive/testAllTwitterFire.py") | Trace-Word -words "fire"
-    } elseif($userInput -like "2") {
-        Invoke-Expression($PipenvRunCommand+" tests/interactive/testListenGeoFires.py")
+    }
+    elseif($userInput -like "1")
+    {
+        Invoke-Expression($PipenvRunCommand + " tests/interactive/testAllTwitterFire.py") | Trace-Word -words "fire"
+    }
+    elseif($userInput -like "2")
+    {
+        Invoke-Expression($PipenvRunCommand + " tests/interactive/testListenGeoFires.py")
     }
 
     Write-Host("q) Quits program.")
