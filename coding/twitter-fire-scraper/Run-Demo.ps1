d640e10ed8b227eb2c0a0f3748326450b258e238
Henry Post <HenryFBP@gmail.com>
2019-02-26 22:29:21 -0600

    make error about py2 clearer

--- coding/twitter-fire-scraper/Run-Demo.ps1
+++ coding/twitter-fire-scraper/Run-Demo.ps1
@@ -2,31 +2,35 @@
 This file runs a demo of the Twitter fire scraper functionality for presentation purposes.
 #>
 
-function detectPython2() {
+function detectPython2([string]$Verbose) {
     # Detects Python2 command.
 
     $PythonCommand = "python"
-    $PythonVersion = Invoke-Expression ($PythonCommand+" -V")
-
-    Write-Host $PythonVersion
+    $PythonVersion = Invoke-Expression ($PythonCommand+" -V") 
 
     if($PythonVersion -like "Python 2") {
 
-        Write-Host "Python 2 detected via ``python`` command."
+        Write-Output("Python 2 detected via ``python`` command.")
 
     } elseif(Get-Command "python2" -ErrorAction SilentlyContinue) { # `python` is not Py2,
 
         $PythonCommand = "python2";
-        Write-Host("Python 2 detected via ``python2`` command.")
+        Write-Output("Python 2 detected via ``python2`` command.")
     } else { # `python2` does not exist.
 
-        Write-Host("``python`` refers to Python 3 and ``python2`` does not exist.")
+        Write-Output("``python`` refers to Python 3 and ``python2`` does not exist.`n")
 
-        Write-Host("Consider putting Python 2 on the PATH environment variable (order matters),")
+        Write-Output("Consider putting Python 2 on the PATH environment variable (order matters),")
 
-        Write-Host("or make a ``python2.exe`` executable available in a folder that is on the PATH.")
+        Write-Output("or make a ``python2.exe`` executable available in a folder that is on the PATH.`n")
 
-        Get-Command "python" | Format-Table Path, Name
+        Write-Output("Running ``where python``")
+
+        Write-Output("This is the current location of ``python``:")
+
+        Get-Command ("python") | Format-Table Path, Name
+
+        Write-Output("Exiting. This demo will not work until these issues are resolved.")
 
         Exit(1)
     }
