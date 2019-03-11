cd865c76447868e1741cb8dc4a13c012ef765660
Henry Post <HenryFBP@gmail.com>
2019-03-11 14:34:05 -0500

    add installation of virtual Python, and installation of our package
    add installation of virtual Python, and installation of our package off
    of the test site.

--- coding/twitter-fire-scraper/test-download.rb
+++ coding/twitter-fire-scraper/test-download.rb
@@ -1,4 +1,4 @@
-# This file will attempt to download, install, and run tests on the uploaded package from PyPI.
+# This file will attempt to download, install, and run tests on the uploaded test package from PyPI.
 #
 require 'fileutils'
 
@@ -20,5 +20,40 @@ puts ("MKDIR #{config.temp_folder}")
 # Go into temp folder
 Dir.chdir(config.temp_folder)
 
-puts "and then ya do " + "pip install -i https://test.pypi.org/simple/ twitter-fire-scraper"
-puts "TODO :)"
+# Create virtual environment to download test package
+puts "Creating virtual environment..."
+system("#{config.python_exe} -m virtualenv env")
+
+# Folder that holds the virtualenv.
+venv_folder = File.absolute_path(File.join("env"))
+
+# Detect virtual environment folder location
+venv_python_bin = File.join(venv_folder, "Scripts")
+
+# It's windows if /Scripts/ exists
+if File.exist? venv_python_bin
+  puts "Using /Scripts/ in #{venv_folder}, Windows user."
+end
+
+# Might be Linux/OSX if "Scripts" doesn't exist.
+unless File.exist? venv_python_bin
+  venv_python_bin = File.absolute_path(File.join("bin"))
+  puts "Using /bin/ in #{venv_folder}, *NIX user."
+end
+
+# Still doesn't exist? Exit.
+unless File.exist? venv_python_bin
+  puts "Could not detect `Scripts` or `bin` folder. Something is terribly wrong."
+  exit(1)
+end
+
+# Path to venv Python executable.
+virtual_python_exe = File.join(venv_python_bin, "python")
+
+# Print venv Python version for sanity.
+system("#{virtual_python_exe} -V")
+
+# Install test package.
+system("#{virtual_python_exe} -m pip install -i https://test.pypi.org/simple/ twitter-fire-scraper")
+
+# TODO run tests now that it's installed!
