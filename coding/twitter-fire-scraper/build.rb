fcb705d1006402f09188a64ed5b9e63dc2d12011
HenryFBP <HenryFBP@gmail.com>
2019-03-11 19:46:24 -0500

    make sure wheel is installed in build step

--- coding/twitter-fire-scraper/build.rb
+++ coding/twitter-fire-scraper/build.rb
@@ -5,4 +5,8 @@ config = Config.new
 
 Dir.chdir("src")
 
+# Ensure wheel is installed.
+system("#{config.python_exe} -m pip install wheel setuptools")
+
+# Package wheel from source.
 system("#{config.python_exe} setup.py sdist bdist_wheel")
