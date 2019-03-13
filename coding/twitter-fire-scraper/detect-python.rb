971b0f1a11bda926d1073e423aba7a41f91ab0fe
Henry Post <HenryFBP@gmail.com>
2019-03-13 15:49:08 -0500

    fully generify detect-python.rb

--- coding/twitter-fire-scraper/detect-python.rb
+++ coding/twitter-fire-scraper/detect-python.rb
@@ -10,7 +10,7 @@ def detect_python_exe(verbose = nil, version="2")
     stdout, stderr, status = Open3.capture3("python -V")
     python_version_info = stdout + stderr
 
-    # If `python` refers to Python X,
+    # If `python` refers to Python version X,
     if python_version_info =~ /Python #{version}/i
 
       if verbose
@@ -18,7 +18,7 @@ def detect_python_exe(verbose = nil, version="2")
       end
 
       return 'python'
-    else # `python` does NOT refer to Python 2. Check for `python2` command.
+    else # `python` does NOT refer to Python version X. Check for `pythonX` command.
 
       if verbose
         puts "`python` exists but doesn't refer to Python #{version}."
@@ -31,7 +31,7 @@ def detect_python_exe(verbose = nil, version="2")
   end
 
 
-  # If `python2` exists,
+  # If `pythonX` exists,
   if find_executable("python#{version}")
 
     stdout, stderr, status = Open3.capture3("python#{version} -V")
@@ -40,12 +40,12 @@ def detect_python_exe(verbose = nil, version="2")
     if python_version_info =~ /Python #{version}/i
 
       if verbose
-        puts "`python#{version}` refers to Python 2!"
+        puts "`python#{version}` refers to Python #{version}!"
       end
 
       return "python#{version}"
     else
-      puts "`python#{version}` command somehow doesn't refer to Python 2..."
+      puts "`python#{version}` command somehow doesn't refer to Python #{version}..."
       puts "Version info: #{python_version_info}"
     end
 
