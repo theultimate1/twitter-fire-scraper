5893435a112c51f06e24deea59f0b546066e2fe8
Henry Post <HenryFBP@gmail.com>
2019-03-13 10:39:40 -0500

    version bump for updated readme

--- coding/twitter-fire-scraper/src/setup.py
+++ coding/twitter-fire-scraper/src/setup.py
@@ -5,7 +5,7 @@ with open("../README.md", 'r') as f:
 
 setuptools.setup(
     name="twitter-fire-scraper",
-    version="0.0.9",
+    version="0.0.10",
 
     author="Henry Post",
     author_email="HenryFBP@gmail.com",
