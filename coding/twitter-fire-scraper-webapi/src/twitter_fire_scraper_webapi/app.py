1931e6bef9a0679be18d785f99e283ae6be3b96d
Trung <phamanhtrung@gmail.com>
2019-03-27 17:56:04 -0500

    add scrape API routing method

--- coding/twitter-fire-scraper-webapi/src/twitter_fire_scraper_webapi/app.py
+++ coding/twitter-fire-scraper-webapi/src/twitter_fire_scraper_webapi/app.py
@@ -67,8 +67,44 @@ def scrape_accounts():
     results = jsonify_status_dict(results)
 
     return jsonify(results)
-    ### ERROR: protected tweets causes 'Not Authorized'
     # return jsonify("You want {} tweets for {}?".format(count, term))
+
+
+@app.route('/scrape', methods=['GET'])
+def scrape():
+    count = request.args.get("count")
+    if not count:
+        abort(400, "'count' is a required URL parameter!")
+    try:
+        count = int(count)
+        if count < 0:
+            raise ValueError
+    except ValueError:
+        abort(400, "'count' should be a valid number!")
+
+    terms = request.args.get("terms")
+    if not terms:
+        abort(400, "'terms' is a required URL parameter!")
+    terms = terms.split(",")
+    if len(terms) <= 0:
+        abort(400, "'terms' cannot be an empty list!")
+
+    accounts = request.args.get("accounts")
+    if not accounts:
+        abort(400, "'accounts' is a required URL parameter!")
+    accounts = accounts.split(",")
+    if len(accounts) <= 0:
+        abort(400, "'accounts' cannot be an empty list!")
+
+    geocode = request.args.get("geocode")
+
+    results = (scraper.scrape(terms=terms, accounts=accounts, count=count, geocode=geocode))
+
+    results = jsonify_status_dict(results)
+
+    return jsonify(results)
+    # return jsonify("You want {} tweets for {}?".format(count, term))
+
 
 @app.route('/info', methods=['GET'])
 def info(): # function: check webapi is running or not
