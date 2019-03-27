653d217b80566aa3936902fb1261b975a41f2aea
Henry Post <HenryFBP@gmail.com>
2019-03-27 13:20:28 -0500

    expose scrape_terms via HTTP

--- coding/twitter-fire-scraper-webapi/src/twitter_fire_scraper_webapi/app.py
+++ coding/twitter-fire-scraper-webapi/src/twitter_fire_scraper_webapi/app.py
@@ -1,20 +1,44 @@
 import os
 from flask import Flask, request, render_template, jsonify, url_for, abort, Response
 import twitter_fire_scraper
-
+from twitter_fire_scraper.scraper import Scraper
+from twitter_fire_scraper.twitter import TwitterAuthentication
+from twitter_fire_scraper.util import jsonify_status_dict
 
 app = Flask(__name__, static_url_path="/static")
-
+scraper = Scraper(twitter_authentication=TwitterAuthentication.autodetect_twitter_auth())
 
 @app.route('/scrape_terms', methods=['GET'])
 def scrape_terms():
 
     count = request.args.get("count")
-
     if not count:
         abort(400, "'count' is a required URL parameter!")
 
-    return jsonify("You want {} tweets?".format(count))
+    try:
+        count = int(count)
+
+        if count < 0:
+            raise ValueError
+    except ValueError:
+        abort(400, "'count' should be a valid number!")
+
+    terms = request.args.get("terms")
+
+    if not terms:
+        abort(400, "'terms' is a required URL parameter!")
+
+    terms = terms.split(",")
+    if len(terms) <= 0:
+        abort(400, "'terms' cannot be an empty list!")
+
+    geocode = request.args.get("geocode")
+
+    results = (scraper.scrape_terms(terms=terms, count=count, geocode=geocode))
+
+    results = jsonify_status_dict(results)
+
+    return jsonify(results)
 
 @app.route('/info', methods=['GET'])
 def info():
