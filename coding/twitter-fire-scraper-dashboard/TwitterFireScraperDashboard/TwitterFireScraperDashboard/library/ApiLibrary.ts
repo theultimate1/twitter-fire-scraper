3b6b60a2092d6e39b2dbdf21889eab870445b14b
Henry Post <HenryFBP@gmail.com>
2019-03-27 16:58:55 -0500

    extract check_api to its own library

---
+++ coding/twitter-fire-scraper-dashboard/TwitterFireScraperDashboard/TwitterFireScraperDashboard/library/ApiLibrary.ts
@@ -0,0 +1,34 @@
+﻿import request = require('request');
+import url = require('url')
+
+export class ApiLibrary {
+    /**
+ * @param host Host of web API.
+ * @param port Port of web API.
+ * @param path Path to web API info.
+ * 
+ * Checks if the twitter-fire-scraper-webapi is functioning correctly.
+ */
+    static check_api(host: string = "http://127.0.0.1", port: string = "3620", path: string = "info") {
+        var uri = new url.URL(host)
+        uri.port = port.toString()
+        uri.pathname = path
+
+        return new Promise((resolve, reject) => {
+            request(uri.href, (err, res, body) => {
+                if (err) {
+                    resolve(false)
+                }
+
+                if (body === "twitter-fire-scraper-webapi") {
+                    console.log("It's the real API.")
+                    resolve(true)
+                }
+                else {
+                    resolve(false)
+                }
+            })
+        })
+    }
+
+}
--- coding/twitter-fire-scraper-dashboard/TwitterFireScraperDashboard/TwitterFireScraperDashboard/routes/index.ts
+++ coding/twitter-fire-scraper-dashboard/TwitterFireScraperDashboard/TwitterFireScraperDashboard/routes/index.ts
@@ -1,44 +1,15 @@
-﻿/*
+﻿/// <reference path="../library/ApiLibrary.ts" />
+/*
  * GET home page.
  */
 import express = require('express');
-import request = require('request');
-import url = require('url')
+import { ApiLibrary } from '../library/ApiLibrary';
 const router = express.Router();
-
-/**
- * @param host Host of web API.
- * @param port Port of web API.
- * @param path Path to web API info.
- * 
- * Checks if the twitter-fire-scraper-webapi is functioning correctly.
- */
-function check_api(host = "http://127.0.0.1", port = "3620", path = "info") {
-    var uri = new url.URL(host)
-    uri.port = port.toString()
-    uri.pathname = path
-
-    return new Promise((resolve, reject) => {
-        request(uri.href, (err, res, body) => {
-            if (err) {
-                resolve(false)
-            }
-
-            if (body === "twitter-fire-scraper-webapi") {
-                console.log("It's the real API.")
-                resolve(true)
-            }
-            else {
-                resolve(false)
-            }
-        })
-    })
-}
 
 router.get('/', async function(req: express.Request, res: express.Response) {
 
 
-    var result = await check_api()
+    var result = await ApiLibrary.check_api()
 
     if (result === null) {
         throw "Async code failed to run! Why???"
