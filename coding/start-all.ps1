577522779662a9fcb7a13202467eb0edeac3fac5
HenryFBP <HenryFBP@gmail.com>
2019-04-02 20:10:05 -0500

    use docstring in start-all.ps1

--- coding/start-all.ps1
+++ coding/start-all.ps1
@@ -1,3 +1,5 @@
+Write-Output "This script will start all servers."
+
 $root_dir=Resolve-Path "./"
 $webapi_dir=Join-Path -Path $root_dir -ChildPath ".\twitter-fire-scraper-webapi\src\twitter_fire_scraper_webapi"
 $dashboard_dir=Join-Path -Path $root_dir -ChildPath ".\twitter-fire-scraper-dashboard\TwitterFireScraperDashboard\TwitterFireScraperDashboard"
