0f2a2abccdfd9a6d073b19cac93b1f911c7d6153
Henry Post <HenryFBP@gmail.com>
2019-04-10 17:49:50 -0500

    add utility class for reusing tweets for test cases

---
+++ coding/twitter-fire-scraper/src/twitter_fire_scraper/tests/test/incur_api_hits/cached_tweets.py
@@ -0,0 +1,39 @@
+"""
+This is a utility to provide a large amount of tweets to unit tests WITHOUT repeatedly incurring API hits.
+
+The idea here is this file gets run once and its data used repeatedly, instead of scraping the same terms again and again.
+
+This is not strictly necessary, but will decrease API calls significantly if you don't care about reusing tweets.
+"""
+from typing import Dict, List
+
+from tweepy import Status
+
+from twitter_fire_scraper.scraper import TwitterAuthentication, Scraper
+
+from functools import lru_cache
+
+class CachedTweets:
+
+    scraper = Scraper(twitter_authentication=TwitterAuthentication.autodetect_twitter_auth())
+
+    @staticmethod
+    @lru_cache
+    def tweets_small():
+        # type: () -> Dict[str, List[Status]]
+        """Return a static list of 9 tweets that is generated once and re-used throughout the module's lifetime."""
+        return CachedTweets.scraper.scrape_terms({"flood", "fire", "house fire"}, count=3)
+
+    @staticmethod
+    @lru_cache
+    def tweets_medium():
+        # type: () -> Dict[str, List[Status]]
+        """Return a static list of 60 tweets that is generated once and re-used throughout the module's lifetime."""
+        return CachedTweets.scraper.scrape_terms({"flood", "fire", "house fire"}, count=20)
+
+    @staticmethod
+    @lru_cache
+    def tweets_large():
+        # type: () -> Dict[str, List[Status]]
+        """Return a static list of 300 tweets that is generated once and re-used throughout the module's lifetime."""
+        return CachedTweets.scraper.scrape_terms({"flood", "fire", "house fire"}, count=100)
