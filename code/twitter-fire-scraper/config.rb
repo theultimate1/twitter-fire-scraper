4979c7768bb2dffac9958de2d700e744c77680fb
Henry Post <henryfbp@gmail.com>
2019-05-15 19:17:24 -0500

    fix Ruby args being passed as a dict

--- code/twitter-fire-scraper/config.rb
+++ code/twitter-fire-scraper/config.rb
@@ -44,12 +44,12 @@ class Config
 
   def python_exe
     begin
-      return detect_python_exe(version: 3)
+      return detect_python_exe(true, 3)
     rescue
       puts "Looks like Python 3 isn't installed. Let's try to install it."
       try_install_python(3)
 
-      return detect_python_exe(version: 3)
+      return detect_python_exe(true, 3)
     end
   end
 
