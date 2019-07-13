5e55971bf3863c0b8d71dc71daf65c8f6ebafa72
HenryFBP <HenryFBP@gmail.com>
2019-07-13 16:16:54 -0500

    flesh out stub classes slightly

--- code/twitter-fire-scraper/twitter_fire_scraper/database/TweetResult.py
+++ code/twitter-fire-scraper/twitter_fire_scraper/database/TweetResult.py
@@ -6,8 +6,15 @@ class TweetResult(object):
 
     def __init__(self, data: Status, tags: Set[str], relevancy: bool):
         self.data = data
+        self.id = self.data.id
         self.tags = tags
         self.relevancy = relevancy
 
-    def get_id(self):
-        return self.data.id
+    def serialize_to_dict(self):
+        """Serialize this TweetResult object to a dictionary that can be saved in a BSON format (like JSON)"""
+        return {
+            'data': self.data,
+            'id': self.id,
+            'tags': self.tags,
+            'relevancy': self.relevancy
+        }
--- code/twitter-fire-scraper/twitter_fire_scraper/database/TweetResultDAO.py
+++ code/twitter-fire-scraper/twitter_fire_scraper/database/TweetResultDAO.py
@@ -1,4 +1,12 @@
+from flask_pymongo import PyMongo, MongoClient
+
+from twitter_fire_scraper.database import TweetResult
+
 class TweetResultDAO(object):
-    def __init__(self, cstr: str):
-        # TODO initialize connection to database
-        pass
+
+    def __init__(self, mongoclient: MongoClient):
+        self.mongoclient = mongoclient
+        self.connection = self.mongoclient.tweets
+
+    def save_one(self, collection: str, tweetresult: TweetResult):
+        self.connection[collection].save(tweetresult)
