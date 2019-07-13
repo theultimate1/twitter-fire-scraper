c25dbfec41617e64f834ff9b5730ee8e1411955e
HenryFBP <HenryFBP@gmail.com>
2019-07-13 16:18:09 -0500

    use constant collection name

--- code/twitter-fire-scraper/twitter_fire_scraper/database/TweetResultDAO.py
+++ code/twitter-fire-scraper/twitter_fire_scraper/database/TweetResultDAO.py
@@ -5,8 +5,11 @@ from twitter_fire_scraper.database import TweetResult
 class TweetResultDAO(object):
 
     def __init__(self, mongoclient: MongoClient):
+
+        self.collection_name = "tweets"
+
         self.mongoclient = mongoclient
         self.connection = self.mongoclient.tweets
 
-    def save_one(self, collection: str, tweetresult: TweetResult):
-        self.connection[collection].save(tweetresult)
+    def save_one(self, tweetresult: TweetResult):
+        self.connection[self.collection_name].save(tweetresult)
