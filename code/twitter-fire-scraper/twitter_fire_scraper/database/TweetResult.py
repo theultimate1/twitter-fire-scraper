410565009636fdf4acc5b82498b7237c0b825634
HenryFBP <HenryFBP@gmail.com>
2019-07-22 19:32:56 -0500

    model TweetResult further

--- code/twitter-fire-scraper/twitter_fire_scraper/database/TweetResult.py
+++ code/twitter-fire-scraper/twitter_fire_scraper/database/TweetResult.py
@@ -1,20 +1,45 @@
+from enum import Enum
+
 from tweepy import Status
 from typing import Set
 
 
+class ERelevancy(Enum):
+    """An enumeration that represents if something is uncategorized, relevant, or irrelevant."""
+    UNCATEGORIZED = 1
+    RELEVANT = 2
+    IRRELEVANT = 3
+
+
 class TweetResult(object):
 
-    def __init__(self, data: Status, tags: Set[str], relevancy: bool):
+    def __init__(self, data: Status, tags: Set[str], relevancy: ERelevancy):
         self.data = data
-        self.id = self.data.id
         self.tags = tags
         self.relevancy = relevancy
 
+    def get_id(self):
+        """The ID of a Tweet. This is unique."""
+        return self.data.id
+
+    def get_text(self):
+        """The content of a tweet.
+
+        Returns either "full_text" or "text", favoring the former as it is longer."""
+        if 'full_text' in self.data:
+            return self.data['full_text']
+
+        elif 'text' in self.data:
+            return self.data['text']
+
+        else:
+            raise KeyError("Tweet has no text!")
+
     def serialize_to_dict(self):
-        """Serialize this TweetResult object to a dictionary that can be saved in a BSON format (like JSON)"""
+        """Serialize this TweetResult object to a JSON-like object that can be saved to a MongoDB database"""
         return {
             'data': self.data,
-            'id': self.id,
+            '_id': self.get_id(),
             'tags': self.tags,
             'relevancy': self.relevancy
         }
